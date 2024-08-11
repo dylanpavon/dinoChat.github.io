ecece52f1bd243d16e92132706f71b57cc326fd7
Dylan <dpavon217@gmail.com>
2024-08-11 09:10:06 -0300

    Cambios EvenLabs

--- dinoWeb.py
+++ dinoWeb.py
@@ -2,9 +2,17 @@ from openai import OpenAI
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify  
 import json
+from typing import IO
+from io import BytesIO
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
 
 
-# Obtener la clave de API desde la variable de entorno
+# Obtener la clave de API OPENAI y ELEVENLABS desde la variable de entorno
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+client = ElevenLabs(
+    api_key=ELEVENLABS_API_KEY,
+)
 api_key = os.getenv("OPENAI_API_KEY")
 client= OpenAI()
 OpenAI.api_key = os.getenv("OPENAI_API_KEY")
@@ -111,6 +119,34 @@ def chatear(id):
         return jsonify({"pregunta": pregunta, "respuesta": respuesta})
             
     return jsonify({"error": "Método no permitido"}), 405
+
+def texto_a_audio(text: str) -> IO[bytes]:
+    # Perform the text-to-speech conversion
+    response = client.text_to_speech.convert(
+        voice_id="D38z5RcWu1voky8WS1ja", # Voz de Fin
+        output_format="mp3_22050_32",
+        text=text,
+        model_id="eleven_multilingual_v2",
+        voice_settings=VoiceSettings(
+            stability=0.0,
+            similarity_boost=1.0,
+            style=0.0,
+            use_speaker_boost=True,
+        ),
+    )
+    # Create a BytesIO object to hold the audio data in memory
+    audio_stream = BytesIO()
+
+    # Write each chunk of audio data to the stream
+    for chunk in response:
+        if chunk:
+            audio_stream.write(chunk)
+
+    # Reset stream position to the beginning
+    audio_stream.seek(0)
+
+    # Return the stream for further use
+    return audio_stream
         
 
 if __name__ == "__main__":
